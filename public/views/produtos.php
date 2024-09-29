50bfc6fda9784fa96caf2c128025ffc11d20a290
Gabriel Kleber <gabrielkleber54321n@gmail.com>
2024-09-29 15:24:45 -0300

    páginas de cada tipo de produto

--- public/views/produtos.php
+++ public/views/produtos.php
@@ -1,23 +1,34 @@
-<?php 
+<?php
 
-if(!isset($_GET['produto'])) {
+if (!isset($_GET['produto'])) {
     echo "<script>window.location.href='./'</script>";
 }
 
-$produto = $_GET['produto'];
+$con = mysqli_connect('localhost', 'root', '', 'loja_perifericos');
 
+$categoria = $_GET['produto'];
+
+$categorias = mysqli_fetch_all(mysqli_query($con, "SELECT id FROM categorias WHERE nome='$categoria' LIMIT 1"));
+
+$id_categoria = $categorias[0][0];
+
+$produto = mysqli_query($con, "SELECT * FROM produtos WHERE categoria='$id_categoria'");
+
+$produtos = mysqli_fetch_all($produto);
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $_GET['produto']?></title>
+    <title><?= $_GET['produto'] ?></title>
     <link rel="stylesheet" href="public/assets/css/produtos.css">
     <title>Loja de Periféricos</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
+
 <body>
     <header>
         <nav>
@@ -27,8 +38,8 @@ $produto = $_GET['produto'];
                     <li><a href="cadastro.php">Cadastrar</a></li>
                 <?php } else { ?>
                     <li><a href="cadastroprod.php">Cadastrar Produtos</a></li>
-                    <li><a href="./compras.php">Carrinho</a></li>
                 <?php } ?>
+                <li><a href="./compras.php">Minhas Compras</a></li>
                 <li><a href="index.php">Voltar</a></li>
 
             </ul>
@@ -40,7 +51,28 @@ $produto = $_GET['produto'];
     </header>
 
     <section class="products">
-        <h2><?= $produto?></h2>
+        <h2><?= $categoria ?></h2>
+
+        <div class="product-grid">
+            <?php
+            for ($i = 0; $i < mysqli_num_rows($produto); $i++) {
+            ?>
+                <div class="product-card">
+                    <img src="public/assets/img/<?= $produtos[$i][4] ?>" alt="<?= $produtos[$i][1] ?>">
+                    <h3><?= $produtos[$i][1] ?></h3>
+                    <p>R$ <?= number_format($produtos[$i][3], 2, ',', '.') ?></p>
+                    <div class="rating">
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9734;</span>
+                    </div>
+                    <button onclick="window.location.href='./addProd.php&idProd=<?= $produtos[$i][0] ?>'">Comprar</button>
+                </div>
+            <?php } ?>
+        </div>
     </section>
 </body>
+
 </html>
